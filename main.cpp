010c14e0f855d4d600543f4e703ca7aa144c3572
SuchayK <67595987+SuchayK@users.noreply.github.com>
2024-07-02 16:46:22 -0500

    Proportional Term - Move and Turn

--- main.cpp
+++ main.cpp
@@ -18,37 +18,83 @@ IMU imu;
 const int CLICKS_PER_ROTATION = 12;
 const float GEAR_RATIO = 29.86F;
 const float WHEEL_DIAMETER = 3.2;
-const float WHEEL_CIRCUMFERENCE = 10.05; //10.0531
+const float WHEEL_CIRCUMFERENCE = 10.05;
 
 volatile int leftC = 0;
 volatile int rightC = 0;
 
-void forward(int speed, int duration) {
-  motors.setSpeeds(speed, speed);
-  delay(duration);
+void movePID(int target) {
+  float kp = 0.1; 
+  int maxSpeed = 100;
+
+  leftC = 0;
+  rightC = 0;
+  while (leftC < target && rightC < target) {
+    leftC += encoders.getCountsAndResetLeft();
+    rightC += encoders.getCountsAndResetRight();
+
+    int le = target - leftC;
+    int re = target - rightC;
+
+    int lpwr = kp * le;
+    int rpwr = kp * re;
+
+    if (lpwr > maxSpeed) lpwr = maxSpeed;
+    if (rpwr > maxSpeed) rpwr = maxSpeed;
+
+    motors.setSpeeds(lpwr, rpwr);
+
+    display.gotoXY(0, 0);
+    display.print("L:");
+    display.print(leftC);
+    display.gotoXY(0, 1);
+    display.print("R:");
+    display.print(rightC);
+  }
+
   motors.setSpeeds(0, 0);
 }
 
-void turnRight(int speed, int duration) {
-  motors.setSpeeds(speed, -speed);
-  delay(duration);
-  motors.setSpeeds(0, 0);
-}
+void turnPID(int angle, bool turnLeft) {
+  float kp = 0.5; 
+  int maxSpeed = 50;
 
-void turnLeft(int speed, int duration) {
-  motors.setSpeeds(-speed, speed);
-  delay(duration);
+  turnSensorReset();
+
+  while (1) {
+    turnSensorUpdate();
+    int error = angle - (((int32_t)turnAngle >> 16) * 360) >> 16;
+
+    int speed = kp * error;
+    if (speed > maxSpeed) speed = maxSpeed;
+    if (speed < -maxSpeed) speed = -maxSpeed;
+
+    if (turnLeft) {
+      motors.setSpeeds(-speed, speed);
+    } else {
+      motors.setSpeeds(speed, -speed);
+    }
+
+    display.gotoXY(0, 0);
+    display.print("Angle:");
+    display.print((((int32_t)turnAngle >> 16) * 360) >> 16);
+    display.print(F("   "));
+
+    if (abs(error) < 5) break;
+  }
+
   motors.setSpeeds(0, 0);
 }
 
 void setup() {
-  Serial.begin(57600);
+  Serial.begin(7600);
   delay(1000);
 
-  forward(100, 2000);   
-  turnRight(100, 500);  
-  forward(100, 2000);   
-  turnLeft(100, 500);  
+  turnSensorSetup();
+
+  movePID(1800);   
+  turnPID(90, true);  
+
 }
 
 void loop() {
